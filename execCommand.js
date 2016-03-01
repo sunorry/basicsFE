069abc19b4e6c9965ed1a89e17abb7ba838eebbc
sunorry <sunorry@gmail.com>
2016-03-01 19:42:32 +0800

    .

--- execCommand.js
+++ execCommand.js
@@ -1,12 +1,19 @@
 var fs = require('fs'),
     execSync = require('child_process').execSync;
 
+/**
+ * 创建文件
+ * @param  {string} path      file path
+ * @param  {string} content   file content
+ * @param  {string} msg       create down massage
+ * @param  {bool} stringify stringify
+ */
 var createFile = function(path, content, msg, stringify) {
 
     if(!fs.existsSync(path)) {
 
-        execSync('touch ' + path);
-        execSync('chmod 777 ' + path);
+        execSync('touch ' + path); // 创建文件
+        execSync('chmod 777 ' + path); // 改变文件权限
         fs.writeFileSync(path, stringify ? JSON.stringify(content) : content);
         console.log(msg);
 
@@ -15,4 +22,4 @@ var createFile = function(path, content, msg, stringify) {
 
 var filePath = process.cwd() + '/test.txt';
 var content = 'Hello world!';
-createFile(filePath, content, 'down');
+createFile(filePath, content, 'down', true);

8315f9b1bfbbc96db38b66bf631638d6c24524c2
sunorry <sunorry@gmail.com>
2016-03-07 15:48:44 +0800

    add event node

--- node/nodeEvent.js
+++ node/nodeEvent.js
@@ -1,3 +1,4 @@
+// method1
 var EventEmitter = require('events').EventEmitter;
 
 function Dog(name) {
@@ -15,3 +16,29 @@ simon.on('bark', function() {
 
 
 sim.emit('bark');
+
+
+// Node 内置模块 util 的 inherits 方法，提供了另一种继承 EventEmitter 的写法
+
+var util = require('util');
+var EventEmitter = require('events').EventEmitter;
+
+var Radio = function(station) {
+    var self = this;
+
+    setTimeout(function() {
+        self.emit('open', station);
+    }, 0);
+
+    setTimeout(function() {
+        self.emit('close', station);
+    }, 5000);
+
+    this.on('newListener', function(listener) {
+        console.log('Event Listener: ' + listener);
+    });
+}
+
+util.inherits(Radio, EventEmitter);
+
+module.exports = Radio;
